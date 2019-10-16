a8e958291f3eff29f33b0d0e87e5755610b06e39
Alen <Alenslo@gmx.net>
2019-10-16 22:49:11 +0200

    added close functionality

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function createTodo () {
-    let list = document.querySelector('#list');
-    let newTodo = document.createElement('li');
-    
-    list.appendChild(newTodo).innerHTML = document.querySelector('#input-text').value
-}
+    let text = document.querySelector('#input-text').value
+    if (text !== "") {
+        // create a new task and append it
+        let list = document.querySelector('#list');
+        let newTodo = document.createElement('li');
+        list.appendChild(newTodo).innerHTML = text; 
+        document.querySelector('#input-text').value = '';
+
+        // create the close button
+        let closeButton = document.createElement('span');
+        let icon = document.createTextNode('\u00D7');
+        closeButton.className = 'close';
+        closeButton.appendChild(icon);
+        newTodo.appendChild(closeButton);
+        
+
+    } else {
+        alert('Please enter a task');
+    };
+
+    // close functionality
+    closeButtons = document.querySelectorAll('.close');
+    closeButtons.forEach((entries) => {
+        entries.onclick = function() {
+            this.parentNode.style.display = 'none';
+        }
+    });  
+};
+
+
+
